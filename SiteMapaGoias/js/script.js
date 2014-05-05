6edd30970a98edbfe57eef0dd15034507e8b3e98
Alexandre Mota <alexandremota.es@outlook.com>
2014-05-04 23:56:18 -0300

    criando o script

--- SiteMapaGoias/js/script.js
+++ SiteMapaGoias/js/script.js
@@ -1,3 +1,13 @@
 /**
  * @author Alexandre
  */
+
+function mudarCor() {
+  
+  
+  var obj = document.getElementById("mapa");
+  var elem = document.getElementById()
+  var elem = obj.getAttribute()
+  
+  
+}
