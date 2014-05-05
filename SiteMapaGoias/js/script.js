d8d93cb12523432c4d838efc4c3e0b8a58977a00
Alexandre Mota <alexandremota.es@outlook.com>
2014-05-05 02:07:21 -0300

    Corrigindo erros

--- SiteMapaGoias/js/script.js
+++ SiteMapaGoias/js/script.js
@@ -1,17 +1,11 @@
 /**
  * @author Alexandre
  */
-<<<<<<< HEAD
 
-function mudarCor() {
+ function mudarCor(){ 
+ var obj = document.getElementById("mapaSVG").getSVGDocument();
+  //document.getElementById("path114");
+  var municipios = obj.getElementsByClassName();
   
-  
-  var obj = document.getElementById("mapa");
-  var elem = document.getElementById()
-  var elem = obj.getAttribute()
-  
-=======
-function carregarSVG () {
->>>>>>> e7e5e86f97c40f3b95cac43d17aa899c2a395cf7
-  
-}
+  alert(obj); 
+} 
