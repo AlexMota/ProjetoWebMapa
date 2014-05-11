5858426a7ddb77737f857532bc75a5cddb8a9ab9
Alexandre Mota <alexandremota.es@outlook.com>
2014-05-11 17:10:20 -0300

    Colocando os event no script

--- SiteMapaGoias/js/script.js
+++ SiteMapaGoias/js/script.js
@@ -2,11 +2,28 @@
  * @author Alexandre
  */
 
- function mudarCor(){ 
- var mapaSVG = document.getElementById("mapa").contentDocument;
-  var municipio = mapaSVG.getElementById("path44");
-  municipio.onmouseover = function () {
-    alert("Ha!");
-   //municipio.style.fill = "lime";
-  }; 
+function mudarCor() {
+	var mapaSVG = document.getElementById("mapa").contentDocument;
+
+	var cidades = new Array(246);
+
+	//var cont = 0;
+	var estilo;
+
+	for (var i = 0; i < 246; i++) {
+
+		cidades[i] = mapaSVG.getElementById("mun_" + (i + 1));
+
+		cidades[i].addEventListener("mouseover", function() {
+			estilo = this.getAttribute("style");
+			this.style.fill = "black";
+		}, false);
+		
+		cidades[i].addEventListener("mouseout", function() {
+			this.style = estilo;
+		}, false);
+		
+
+	}
+
 } 
