68849e98a5fa08e1a012bf813937a2095801da88
Alexandre Mota <alexandremota.es@outlook.com>
2014-05-11 22:41:00 -0300

    Corrigindo problemas e apagando mapas desnecessários

--- SiteMapaGoias/js/script.js
+++ SiteMapaGoias/js/script.js
@@ -7,22 +7,21 @@ function mudarCor() {
 
 	var cidades = new Array(246);
 
-	//var cont = 0;
-	var estilo;
+	
 
 	for (var i = 0; i < 246; i++) {
 
 		cidades[i] = mapaSVG.getElementById("mun_" + (i + 1));
 
 		cidades[i].addEventListener("mouseover", function() {
-			estilo = this.getAttribute("style");
+			this.style.color = this.style.fill;
+
 			this.style.fill = "black";
 		}, false);
-		
+
 		cidades[i].addEventListener("mouseout", function() {
-			this.style = estilo;
+			this.style.fill = this.style.color;;
 		}, false);
-		
 
 	}
 
